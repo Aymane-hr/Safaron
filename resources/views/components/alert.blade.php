a8aeeb12fdbf2e87a98080827e580d63660457fc
SoufianeOuenjli <soufianounjli@gmail.com>
2025-03-09 00:58:57 +0000

    Fix the alert to succes and error style.

--- resources/views/components/alert.blade.php
+++ resources/views/components/alert.blade.php
@@ -1,28 +1,35 @@
 @props(['type', 'message'])
 
 <script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const type = '{{ $type }}';  // Get the message type from Blade
+        const isError = type === 'error';
+
         const Toast = Swal.mixin({
-        toast: true,
-        position: "bottom-end",
-        showConfirmButton: false,
-        background: "#a80000",
-        color: "#fff",
-        iconColor: "#fff",
-        timer: 5000,
-        timerProgressBar: true,
-        timerProgressBarColor: "#fff",
-        didOpen: (toast) => {
-            toast.onmouseenter = Swal.stopTimer;
-            toast.onmouseleave = Swal.resumeTimer;
-            let progressBar = toast.querySelector('.swal2-timer-progress-bar');
-            if (progressBar) {
-                progressBar.style.backgroundColor = "#fff";  // Change to your desired color
+            toast: true,
+            position: "bottom-end",
+            showConfirmButton: false,
+            background: isError ? "#a80000" : "",
+            color: isError ? "#fff" : "",
+            iconColor: isError ? "#fff" : "",
+            timer: 5000,
+            timerProgressBar: true,
+            didOpen: (toast) => {
+                toast.onmouseenter = Swal.stopTimer;
+                toast.onmouseleave = Swal.resumeTimer;
+
+                if (isError) {
+                    let progressBar = toast.querySelector('.swal2-timer-progress-bar');
+                    if (progressBar) {
+                        progressBar.style.backgroundColor = "#fff"; // White progress bar for error
+                    }
+                }
             }
-        }
         });
+
         Toast.fire({
-        icon: '{{ $type }}',
-        title: '{{ $message }}'
+            icon: type,
+            title: '{{ $message }}'
         });
+    });
 </script>
-<p style="color: #a80000"></p>
