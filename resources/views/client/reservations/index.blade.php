1418603a2845d869db10f4d050b6ed884a4b8d1f
yassinemchachti <yassineabakhti11@gmail.com>
2025-03-10 20:19:58 +0000

    feat: update reservation image source to use dynamic asset path

--- resources/views/client/reservations/index.blade.php
+++ resources/views/client/reservations/index.blade.php
@@ -62,7 +62,7 @@
                                 </h4>
                                 <div class="d-flex align-items-center flex-wrap">
                                     <p class="fs-14 mb-2 me-3 pe-3 border-end d-flex align-items-center">
-                                        <img src="https://images.unsplash.com/photo-1587691592099-24045742c181?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="me-2" alt="Img">
+                                        <img src="{{ asset('storage/' . $voyage->autocar->image)  }}" class="me-2" alt="Img">
                                     </p>
                                 </div>
                             </div>
