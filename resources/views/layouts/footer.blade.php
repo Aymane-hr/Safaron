ef1a4805e79b46d976b6e844a3e26c5ba7ba95ce
SoufianeOuenjli <soufianounjli@gmail.com>
2025-03-11 23:03:08 +0000

    Translate footer content to French; update links and labels

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -9,19 +9,19 @@
                                 <h5>Pages</h5>
                                 <ul class="footer-menu">
                                     <li>
-                                        <a href="team.html">Our Team</a>
+                                        <a href="team.html">Notre Équipe</a>
                                     </li>
                                     <li>
-                                        <a href="pricing-plan.html">Pricing Plans</a>
+                                        <a href="pricing-plan.html">Tarifs</a>
                                     </li>
                                     <li>
-                                        <a href="gallery.html">Gallery</a>
+                                        <a href="gallery.html">Galerie</a>
                                     </li>
                                     <li>
-                                        <a href="profile-settings.html">Settings</a>
+                                        <a href="profile-settings.html">Paramètres</a>
                                     </li>
                                     <li>
-                                        <a href="my-profile.html">Profile</a>
+                                        <a href="my-profile.html">Mon Profil</a>
                                     </li>
                                     <li>
                                         <a href="agent-listings.html">Listings</a>
@@ -31,25 +31,25 @@
                         </div>
                         <div class="col">
                             <div class="footer-widget">
-                                <h5>Company</h5>
+                                <h5>Entreprise</h5>
                                 <ul class="footer-menu">
                                     <li>
-                                        <a href="about-us.html">About Us</a>
+                                        <a href="about-us.html">À Propos de Nous</a>
                                     </li>
                                     <li>
-                                        <a href="javascript:void(0);">Careers</a>
+                                        <a href="javascript:void(0);">Carrières</a>
                                     </li>
                                     <li>
                                         <a href="blog-grid.html">Blog</a>
                                     </li>
                                     <li>
-                                        <a href="javascript:void(0);">Affiliate Program</a>
+                                        <a href="javascript:void(0);">Programme d'affiliation</a>
                                     </li>
                                     <li>
-                                        <a href="add-flight.html">Add Your Listing</a>
+                                        <a href="add-flight.html">Ajouter votre listing</a>
                                     </li>
                                     <li>
-                                        <a href="javascript:void(0);">Our Partners</a>
+                                        <a href="javascript:void(0);">Nos Partenaires</a>
                                     </li>
                                 </ul>
                             </div>
@@ -59,7 +59,7 @@
                                 <h5>Destinations</h5>
                                 <ul class="footer-menu">
                                     <li>
-                                        <a href="javascript:void(0);">Hawai</a>
+                                        <a href="javascript:void(0);">Hawaï</a>
                                     </li>
                                     <li>
                                         <a href="javascript:void(0);">Istanbul</a>
@@ -68,10 +68,10 @@
                                         <a href="javascript:void(0);">San Diego</a>
                                     </li>
                                     <li>
-                                        <a href="javascript:void(0);">Belgium</a>
+                                        <a href="javascript:void(0);">Belgique</a>
                                     </li>
                                     <li>
-                                        <a href="javascript:void(0);">Newyork</a>
+                                        <a href="javascript:void(0);">New York</a>
                                     </li>
                                     <li>
                                         <a href="javascript:void(0);">Los Angeles</a>
@@ -81,25 +81,24 @@
                         </div>
                         <div class="col">
                             <div class="footer-widget">
-                                <h5>Quick Links</h5>
+                                <h5>Liens Rapides</h5>
                                 <ul class="footer-menu">
                                     <li>
-                                        <a href="contact-us.html">Contact Us</a>
+                                        <a href="contact-us.html">Contactez-nous</a>
                                     </li>
                                     <li>
-                                        <a href="javascript:void(0);">Legal Notice</a>
+                                        <a href="javascript:void(0);">Avis Légales</a>
                                     </li>
                                     <li>
-                                        <a href="privacy-policy.html">Privacy Policy</a>
                                     </li>
                                     <li>
-                                        <a href="terms-conditions.html">Terms and Conditions</a>
+                                        <a href="terms-conditions.html">Termes et Conditions</a>
                                     </li>
                                     <li>
-                                        <a href="chat.html">Chat Support</a>
+                                        <a href="chat.html">Support en Direct</a>
                                     </li>
                                     <li>
-                                        <a href="javascript:void(0);">Refund Policy</a>
+                                        <a href="javascript:void(0);">Politique de Remboursement</a>
                                     </li>
                                 </ul>
                             </div>
@@ -109,24 +108,24 @@
                 <div class="col-xl-5 col-lg-6">
                     <div class="footer-widget">
                         <div class="mb-4">
-                            <h5 class="mb-1">Subscribe to Our Newsletter</h5>
-                            <p class="mb-3">Just sign up and we'll send you a notification by email.</p>
+                            <h5 class="mb-1">Abonnez-vous à Notre Newsletter</h5>
+                            <p class="mb-3">Inscrivez-vous simplement et nous vous enverrons une notification par e-mail.</p>
                             <div class="footer-input">
                                 <div class="input-group align-items-center justify-content-center">
                                     <span class="input-group-text px-1"><i class="isax isax-message-favorite5"></i></span>
-                                    <input type="email" class="form-control" placeholder="Enter Email Address">
-                                    <button type="submit" class="btn btn-primary btn-md">Subscribe</button>
+                                    <input type="email" class="form-control" placeholder="Entrez votre Adresse Email">
+                                    <button type="submit" class="btn btn-primary btn-md">S'abonner</button>
                                 </div>
                             </div>
                         </div>
-                        <h5 class="mb-0">Contact Info</h5>
+                        <h5 class="mb-0">Informations de Contact</h5>
                         <div class="d-sm-flex align-items-center justify-content-center justify-content-between">
                             <div class="d-flex align-items-center justify-content-center justify-content-sm-start me-3 mt-2">
                                 <span class="avatar avatar-lg bg-light rounded-circle flex-shrink-0">
                                     <i class="ti ti-headphones-filled fs-24 text-gray-9"></i>
                                 </span>
                                 <div class="ms-2">
-                                    <p class="fs-14 mb-1">Customer Support</p>
+                                    <p class="fs-14 mb-1">Soutien Client</p>
                                     <h6 class="fw-medium">+1 56589 54598</h6>
                                 </div>
                             </div>
@@ -135,7 +134,7 @@
                                     <i class="ti ti-message-2 fs-24 text-gray-9"></i>
                                 </span>
                                 <div class="ms-2">
-                                    <p class="fs-14 mb-1">Drop Us an Email</p>
+                                    <p class="fs-14 mb-1">Envoyez-nous un Email</p>
                                     <h6 class="fw-medium text-dark"><a href="https://dreamstour.dreamstechnologies.com/cdn-cgi/l/email-protection" class="__cf_email__" data-cfemail="9df4f3fbf2ddf8e5fcf0edf1f8b3fef2f0">[email&#160;protected]</a></h6>
                                 </div>
                             </div>
@@ -151,7 +150,7 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="d-flex align-items-center justify-content-between flex-wrap">
-                        <p>Copyright &copy; 2025. All Rights Reserved, <a href="javascript:void(0);" class="text-primary fw-medium">DreamsTour</a></p>
+                        <p>Tous droits réservés, <a href="javascript:void(0);" class="text-primary fw-medium">DreamsTour</a> 2025</p>
                         <div class="d-flex align-items-center">
                             <ul class="social-icon">
                                 <li>
@@ -173,10 +172,10 @@
                         </div>
                         <ul class="policy-links">
                             <li>
-                                <a href="javascript:void(0);">Legal Notice</a>
+                                <a href="javascript:void(0);">Avis Légales</a>
                             </li>
                             <li>
-                                <a href="javascript:void(0);">Refund Policy</a>
+                                <a href="javascript:void(0);">Politique de Remboursement</a>
                             </li>
                         </ul>
                     </div>
@@ -187,3 +186,4 @@
     <!-- /Footer Bottom -->
 
 </footer>
+
