0384b4af7b168d2629b26b93b2b9ebfef348a1d7
sarachakour <sarachakour45@gmail.com>
2025-05-12 20:35:14 +0100

    Refactor voyage listing layout and remove unused filter options for improved UI

--- resources/views/client/voyages/listevoyage.blade.php
+++ resources/views/client/voyages/listevoyage.blade.php
@@ -117,62 +117,10 @@
             <div class="row">
                 <div class="col-xl-9 col-lg-9">
                     <div class="d-flex align-items-center justify-content-between flex-wrap">
-                        <h6 class="mb-3">Trouvez votre voyages</h6>
+                        <h6 class="mt-4 mb-1">Trouvez votre voyages</h6>
                         <div class="d-flex align-items-center flex-wrap">
-                            {{-- <div class="list-item d-flex align-items-center mb-3">
-                                <a href="flight-grid.html" class="list-icon me-2"><i
-                                        class="isax isax-grid-1"></i></a>
-                                <a href="flight-list.html" class="list-icon active me-2"><i
-                                        class="isax isax-firstline"></i></a>
-                            </div> --}}
-                            {{-- <div class="dropdown mb-3">
-                                <a href="javascript:void(0);" class="dropdown-toggle py-2" data-bs-toggle="dropdown"
-                                    aria-expanded="false">
-                                    <span class="fw-medium text-gray-9">Filtrer par  : </span>Recommended
-                                </a>
-                                <div class="dropdown-menu dropdown-sm">
-                                    <form action="https://dreamstour.dreamstechnologies.com/html/flight-grid.html">
-                                        <h6 class="fw-medium fs-16 mb-3">Sort By</h6>
-                                        <div class="form-check d-flex align-items-center ps-0 mb-2">
-                                            <input class="form-check-input ms-0 mt-0" name="recommend"
-                                                type="checkbox" id="recommend1" checked>
-                                            <label class="form-check-label ms-2" for="recommend1">Recommended</label>
-                                        </div>
-                                        <div class="form-check d-flex align-items-center ps-0 mb-2">
-                                            <input class="form-check-input ms-0 mt-0" name="recommend"
-                                                type="checkbox" id="recommend2">
-                                            <label class="form-check-label ms-2" for="recommend2">Price: low to
-                                                high</label>
-                                        </div>
-                                        <div class="form-check d-flex align-items-center ps-0 mb-2">
-                                            <input class="form-check-input ms-0 mt-0" name="recommend"
-                                                type="checkbox" id="recommend3">
-                                            <label class="form-check-label ms-2" for="recommend3">Price: high to
-                                                low</label>
-                                        </div>
-                                        <div class="form-check d-flex align-items-center ps-0 mb-2">
-                                            <input class="form-check-input ms-0 mt-0" name="recommend"
-                                                type="checkbox" id="recommend4">
-                                            <label class="form-check-label ms-2" for="recommend4">Newest</label>
-                                        </div>
-                                        <div class="form-check d-flex align-items-center ps-0 mb-2">
-                                            <input class="form-check-input ms-0 mt-0" name="recommend"
-                                                type="checkbox" id="recommend5">
-                                            <label class="form-check-label ms-2" for="recommend5">Ratings</label>
-                                        </div>
-                                        <div class="form-check d-flex align-items-center ps-0 mb-0">
-                                            <input class="form-check-input ms-0 mt-0" name="recommend"
-                                                type="checkbox" id="recommend6">
-                                            <label class="form-check-label ms-2" for="recommend6">Reviews</label>
-                                        </div>
-                                        <div
-                                            class="d-flex align-items-center justify-content-end border-top pt-3 mt-3">
-                                            <a href="javascript:void(0);" class="btn btn-light btn-sm me-2">Reset</a>
-                                            <button type="submit" class="btn btn-primary btn-sm">Apply</button>
-                                        </div>
-                                    </form>
-                                </div>
-                            </div> --}}
+                          
+                          
                         </div>
                     </div>
                     <div class="hotel-list">
@@ -188,23 +136,10 @@
                     <!-- Pagination -->
                     <nav class="pagination-nav">
                         <ul class="pagination justify-content-center">
-                            {{-- <li class="page-item disabled">
-                                <a class="page-link" href="javascript:void(0);" aria-label="Previous">
-                                    <span aria-hidden="true"><i class="fa-solid fa-chevron-left"></i></span>
-                                </a>
-                            </li> --}}
+                          
                          
                             {{ $voyages->links() }}
-                            {{-- <li class="page-item"><a class="page-link" href="javascript:void(0);">1</a></li>
-                            <li class="page-item"><a class="page-link" href="javascript:void(0);">2</a></li>
-                            <li class="page-item"><a class="page-link" href="javascript:void(0);">3</a></li>
-                            <li class="page-item active"><a class="page-link" href="javascript:void(0);">4</a></li>
-                            <li class="page-item"><a class="page-link" href="javascript:void(0);">5</a></li>
-                            <li class="page-item">
-                                <a class="page-link" href="javascript:void(0);" aria-label="Next">
-                                    <span aria-hidden="true"><i class="fa-solid fa-chevron-right"></i></span>
-                                </a>
-                            </li> --}}
+                            
                         </ul>
                     </nav>
                     <!-- /Pagination -->
@@ -212,7 +147,7 @@
                 </div>
 
                 <!-- Sidebar -->
-                <div class="col-xl-3 col-lg-3 theiaStickySidebar">
+                <div class="col-xl-3 col-lg-3 theiaStickySidebar mt-5">
                     <div class="card filter-sidebar mb-4 mb-lg-0">
                         <div class="card-header d-flex align-items-center justify-content-between">
                             <h5>Filtrer</h5>
@@ -574,4 +509,42 @@
         });
    
     </script>
+
+    <style>
+        /* Espacement entre les colonnes principales */
+        .content .row > [class^="col-"] {
+            margin-bottom: 24px;
+        }
+
+        /* Espacement autour du formulaire de recherche */
+        .banner-form {
+            margin-bottom: 32px;
+        }
+
+        /* Espacement entre la sidebar et la liste */
+        @media (min-width: 992px) {
+            .col-xl-9 {
+                padding-right: 24px;
+            }
+            .col-xl-3 {
+                padding-left: 24px;
+            }
+        }
+
+        /* Amélioration du symbole entre From et To */
+        .form-info > span {
+            min-width: 40px;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin: 0 8px;
+            font-size: 20px;
+            color: #888;
+            height: 100%;
+        }
+        /* Optionnel : ajuster la taille de l'icône */
+        .form-info > span i {
+            font-size: 22px;
+        }
+    </style>
 </x-app-layout>
