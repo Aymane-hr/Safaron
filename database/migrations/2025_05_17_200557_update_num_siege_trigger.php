bb01c234210ba690ab26707a0f5e159711508b56
AbdelMe <simoelabdellaoui08@gmail.com>
2025-05-17 21:12:56 +0100

    update nbr_siege when the reservation is done

--- database/migrations/2025_05_17_200557_update_num_siege_trigger.php
+++ database/migrations/2025_05_17_200557_update_num_siege_trigger.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
@@ -11,7 +12,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        DB::unprepared('
+            CREATE TRIGGER update_num_siege BEFORE INSERT ON reservations
+            FOR EACH ROW
+            BEGIN
+                UPDATE autocars SET nbr_siege = nbr_siege - 1
+                WHERE id = NEW.autocar_id;
+            END
+        ');
     }
 
     /**
@@ -19,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        DB::unprepared('DROP TRIGGER IF EXISTS update_num_siege');
     }
 };
